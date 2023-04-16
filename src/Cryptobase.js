8492c31f325f00e3496b16b016ed630a97cd452f
mahadkhire <112701821+kiwimped@users.noreply.github.com>
2023-04-16 14:20:42 -0400

    Initial commit

---
+++ src/Cryptobase.js
@@ -0,0 +1,17 @@
+import BackgroundVideo from "./assets/Cryptohub.mp4";
+import { LinkContainer } from "react-router-bootstrap";
+
+import "./styles.css";
+
+export default function Home() {
+  return (
+    <div styl>
+      <h1 className="front-text">Welcome to Crpytohub</h1>
+      <LinkContainer to="/Chart">
+        <video className="Video-player" loop autoPlay muted={true}>
+          <source src={BackgroundVideo} type="video/mp4" />
+        </video>
+      </LinkContainer>
+    </div>
+  );
+}
